f58191ab797d8616bee2f1d847bdf329d44b0bc2
ChunHeng Jen <chjen@kth.se>
2018-09-10 17:53:02 +0200

    a7 finished & a5 updated

--- python/a5.py
+++ python/a5.py
@@ -17,3 +17,64 @@ print(d.check(t, m.monk2test));
 t=d.buildTree(m.monk3, m.attributes);
 print(d.check(t, m.monk3));
 print(d.check(t, m.monk3test));
+
+
+
+
+print ("First Node IG")
+for i in range(0,6):
+	print(d.averageGain(m.monk1, m.attributes[i]));
+
+a5_1 = d.select(m.monk1, m.attributes[4],1)
+a5_2 = d.select(m.monk1, m.attributes[4],2)
+a5_3 = d.select(m.monk1, m.attributes[4],3)
+a5_4 = d.select(m.monk1, m.attributes[4],4)
+
+print ("subset a5_1 IG")
+for i in range(6):
+	print(d.averageGain(a5_1, m.attributes[i]));
+
+print ("subset a5_2 IG")
+for i in range(6):
+	print(d.averageGain(a5_2, m.attributes[i]));
+	
+print ("subset a5_3 IG")
+for i in range(6):
+	print(d.averageGain(a5_3, m.attributes[i]));
+
+print ("subset a5_4 IG")
+for i in range(6):
+	print(d.averageGain(a5_4, m.attributes[i]));
+
+	
+a5_2_a4_1 = d.select(a5_2, m.attributes[3],1)
+a5_2_a4_2 = d.select(a5_2, m.attributes[3],2)
+a5_2_a4_3 = d.select(a5_2, m.attributes[3],3)
+
+a5_3_a6_1 = d.select(a5_3, m.attributes[5],1)
+a5_3_a6_2 = d.select(a5_3, m.attributes[5],2)
+
+a5_4_a1_1 = d.select(a5_4, m.attributes[0],1)
+a5_4_a1_2 = d.select(a5_4, m.attributes[0],2)
+a5_4_a1_3 = d.select(a5_4, m.attributes[0],3)
+
+print(d.mostCommon(a5_1))
+print(d.mostCommon(a5_2))
+print(d.mostCommon(a5_3))
+print(d.mostCommon(a5_4))
+print(" ")
+print(d.mostCommon(a5_2_a4_1))
+print(d.mostCommon(a5_2_a4_2))
+print(d.mostCommon(a5_2_a4_3))
+print(" ")
+print(d.mostCommon(a5_3_a6_1))
+print(d.mostCommon(a5_3_a6_2))
+print(" ")
+print(d.mostCommon(a5_4_a1_1))
+print(d.mostCommon(a5_4_a1_2))
+print(d.mostCommon(a5_4_a1_3))
+
+
+#compare the result with the ID3 algroithm
+t=d.buildTree(m.monk1, m.attributes, 2);
+draw.drawTree(t)
--- python/a7.py
+++ python/a7.py
@@ -11,19 +11,6 @@ def partition(data, fraction):
     breakPoint = int(len(ldata) * fraction)
     return ldata[:breakPoint], ldata[breakPoint:]
 
-
-
-
-monk1train, monk1val = partition(m.monk1, 0.6)
-#monk2train, monk2val = partition(m.monk2, 0.6)
-monk3train, monk3val = partition(m.monk3, 0.6)
-
-t1=d.buildTree(monk1train, m.attributes);
-t3=d.buildTree(monk3train, m.attributes);
-#d.allPruned(t1);
-#d.allPruned(t2);
-
-
 def checkperformance(tree, monk1val):
 	pruned_trees = d.allPruned(tree);
 	t1_better_performance = -1;
@@ -39,10 +26,50 @@ def checkperformance(tree, monk1val):
 	return tree
 
 
-prunedTree = checkperformance(t1, monk1val);
 
-print(d.check(prunedTree, m.monk1test));
-draw.drawTree(prunedTree);
+fraction = [0.3, 0.4, 0.5, 0.6, 0.7, 0.8]
+for i in fraction:
+	print("begin of monk1: " + str(i))
+	for x in range(10):
+		monk1_train, monk1_val = partition(m.monk1, i)
+		#build tree after partition
+		t1 =  d.buildTree(monk1_train, m.attributes);
+		#prun the tree
+		prunedTree = checkperformance(t1, monk1_val);
+		print(d.check(prunedTree, m.monk1test));
+	print("end of monk1: " + str(i))
+
+	print("begin of monk3: " + str(i))
+	for x in range(10):
+		monk3_train, monk3_val = partition(m.monk3, i)
+		#build tree after partition
+		t3 =  d.buildTree(monk3_train, m.attributes);
+		#prun the tree
+		prunedTree = checkperformance(t3, monk3_val);
+		print(d.check(prunedTree, m.monk3test));
+	print("end of monk3: " + str(i))
+
+
+
+
+
+#monk1train, monk1val = partition(m.monk1, 0.6)
+#monk2train, monk2val = partition(m.monk2, 0.6)
+#monk3train, monk3val = partition(m.monk3, 0.6)
+
+#t1=d.buildTree(monk1train, m.attributes);
+#t3=d.buildTree(monk3train, m.attributes);
+#d.allPruned(t1);
+#d.allPruned(t2);
+
+
+
+
+
+#prunedTree = checkperformance(t1, monk1val);
+
+#print(d.check(prunedTree, m.monk1test));
+#draw.drawTree(prunedTree);
 
 #print(d.check(t1, monk1val));
 #print(d.check(t3, monk3val));
